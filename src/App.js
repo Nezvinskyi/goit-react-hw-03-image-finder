495d39af7ba2ed58a0731ab47d39f93893c28dda
Dmitry Nezvinskyi <dmytro@Dmytros-MacBook.local>
2021-04-29 17:53:38 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Searchbar from './components/Searchbar';
+
+const App = () => (
+  <div className="App">
+    <Searchbar></Searchbar>
+  </div>
+);
+
+export default App;
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,11 @@
+const Searchbar = () => (
+	<header className='Searchbar'>
+		<form className="SearchForm">
+			<button type='submit' className='SearchForm-button'></button>
+			<input type='text' className='SearchForm-input'/>
+		</form>
+
+	</header>
+)
+ 
+export default Searchbar;
